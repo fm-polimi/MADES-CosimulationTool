79416622375fdeef08294fbe5647958cc32ff050
michele.sama@gmail.com <michele.sama@gmail.com@cfc6d262-2f0e-3203-1f8c-975ebc2874de>
2011-05-04 09:13:15 +0000

    added class Cosimulator

---
+++ trunk/src/mades/cosimulation/Cosimulator.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package mades.cosimulation;
+
+/**
+ * @author Michele Sama (m.sama@puzzledev.com)
+ * 
+ * Defines the main controller for the co-simulation. The cosimulator
+ * is responsible for initializing the system and the environment and
+ * for supervising the simulation. 
+ *
+ */
+public class Cosimulator {
+
+	/**
+	 * Default constructor.
+	 */
+	public Cosimulator() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- trunk/src/mades/system/SystemConnector.java
+++ trunk/src/mades/system/SystemConnector.java
@@ -3,10 +3,58 @@
  */
 package mades.system;
 
+import java.util.HashMap;
+
 /**
  * @author Michele Sama (m.sama@puzzledev.com)
  *
+ * Defines the interface to connect the system simulator to the Cosimulator.
+ * Implementation of this interface allows the Cosimulator to initialize 
+ * and control the simulated system during the simulation.
  */
 public interface SystemConnector {
 
+	/**
+	 * Initializes the system simulator with a set of configuration
+	 * parameters given by the Cosimulator. This initialization has to be done
+	 * once and only once.
+	 * 
+	 * @param params: A collection of key,value initialization parameters.
+	 */
+	public void initialize(HashMap<String, String> params);
+	
+	/**
+	 * Loads a given initial state to the system. This method is supposed
+	 * to be invoked ad each step of the co-simulation.
+	 * 
+	 * @param system: The initial configuration of the system. 
+	 *         If <code>null</code> the last simulated step is used.
+	 * @param environment: The initial configuration of the environment. 
+	 * @throws IllegalArgumentException: If <code>system</code> is
+	 *         <code>null</code> and if no simulation steps have been 
+	 *         performed.
+	 */
+	public void load(HashMap<String, String> system,
+			HashMap<String, String> environment);
+	
+	/**
+	 * Performs the next step of the simulation.
+	 * 
+	 * @param time: The time to simulate.
+	 * @return The result of the simulation.
+	 * @throws IllegalArgumentException: If time is negative or lesser 
+	 *         than the last simulated step.
+	 */
+	public HashMap<String, String> simulateNext(double time);
+	
+	/**
+	 * Returns the value of the current step of the system. The returned
+	 * value is the same as the last simulated step.
+	 * 
+	 * @return The result of the last performed step of the simulation. If no
+	 *         simulation has been performed it returns an empty collection.
+	 * @throws AssertionError: If no simulation step has been performed.
+	 */
+	public HashMap<String, String> getCurrentParams();
+	
 }
