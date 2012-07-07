143eac2bc37299e2f16e5ba27c7521eac8eb8483
michele.sama <michele.sama@gmail.com>
2012-07-07 22:23:21 +0000

    added a first version of the NuZot adapter. Not yet working

---
+++ src/mades/system/nuzot/NuZotSystemConnector.java
@@ -0,0 +1,79 @@
+/**
+ * 
+ */
+package mades.system.nuzot;
+
+import java.util.logging.Logger;
+
+import mades.common.timing.Clock;
+import mades.common.variables.TriggerFactory;
+import mades.common.variables.VariableFactory;
+import mades.environment.EnvironmentMemento;
+import mades.system.SystemConnector;
+import mades.system.SystemMemento;
+
+/**
+ * @author Michele Sama (m.sama@puzzledev.com)
+ *
+ */
+public class NuZotSystemConnector implements SystemConnector {
+
+	protected Clock clock;
+	protected Logger logger;
+	protected NuZotWrapper wrapper;
+	
+	protected SystemMemento systemMemento;
+	
+	public NuZotSystemConnector(Logger logger) {
+		this.logger = logger;
+	}
+	
+	/* (non-Javadoc)
+	 * @see mades.system.SystemConnector#initialize(java.util.ArrayList, int)
+	 */
+	@Override
+	public SystemMemento initialize(
+			String systemPath, String systemName, 
+			Clock clock,
+			VariableFactory variableFactory,
+			TriggerFactory triggerFactory,
+			SystemMemento systemMemento) {
+		this.clock = clock;
+		
+		wrapper = new NuZotWrapper(systemPath, systemName,
+				clock, variableFactory, triggerFactory,
+				logger);
+		wrapper.initialize(systemMemento);
+		return systemMemento;
+	}
+
+	/* (non-Javadoc)
+	 * @see mades.system.SystemConnector#load(mades.system.SystemMemento, mades.environment.EnvironmentMemento)
+	 */
+	@Override
+	public void load(SystemMemento systemMemento,
+			EnvironmentMemento environmentParams) {
+		this.systemMemento = new SystemMemento(systemMemento);
+		this.systemMemento.update(environmentParams);
+	}
+
+	/* (non-Javadoc)
+	 * @see mades.system.SystemConnector#simulateNext(int)
+	 */
+	@Override
+	public SystemMemento simulateNext() {
+		systemMemento = wrapper.executeSimulationStep(clock.getCurrentTime(), systemMemento);
+		return systemMemento;
+	}
+
+	/* (non-Javadoc)
+	 * @see mades.system.SystemConnector#getCurrentParams()
+	 */
+	@Override
+	public SystemMemento getCurrentParams() {
+		return systemMemento;
+	}
+
+	
+
+}
